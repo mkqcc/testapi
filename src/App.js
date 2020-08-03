8554bd8104c8877b1316403b3d6d88ad6048f133
mkqcc <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-08-03 14:53:51 -0400

    React Lifecycles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json()
+        }).then(data => {
+            console.log(data)
+            this.setState({isLoaded: true, items: data,})
+        })
+    }
+
+    render() {
+        const {isLoaded, items} = this.state;
+        if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div className="App">
+                    <div className="Names">
+                        <ul>
+                            {items.map(item => {
+                                return (
+                                    <li key={item.id}> Name: {item.name} | Username: {item.username} | {' '}
+                                        <a href={`htttps://${item.website}`}>Website</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            )
+        }
+    }
 }
 
-export default App;
+export default App
